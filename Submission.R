2473e4f8af4ee827f6bb33ae4b03c514652ed2ec
Philippe-C <philippe.chiaroni@hec.edu>
2015-05-22 21:29:11 +0200

    Submission

--- Submission.R
+++ Submission.R
@@ -2,9 +2,11 @@ answers <- result
 pml_write_files <- function(x){
         n = length(x)
         for(i in 1:n){
-                filename = paste0("problem_results/problem_id_",i,".txt")
+                filename = paste0("problem_id_",i,".txt")
                 write.table(x[i], file=filename, quote=FALSE,
                             row.names=FALSE, col.names=FALSE)
         }
 }
-pml_write_files(answers)
+
+pml_write_files(answers)## copy paste the r code above first, then run this command 
+## You must have also created a result folder and this folder must be set as your working directory!
